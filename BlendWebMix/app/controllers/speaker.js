3532d2cf296fdfb78660c243b4ff886afb9f6f0d
Benjamin Clay <benj.clay@gmail.com>
2014-10-28 19:50:01 +0100

    ListView added

--- BlendWebMix/app/controllers/speaker.js
+++ BlendWebMix/app/controllers/speaker.js
@@ -1,1 +1,25 @@
 var args = arguments[0] || {};
+
+var data = [
+    { name: "Benjamin Clay", talk: "Titanium", searchable: "benjamin clay ternel titanium alloy", date: "2014-10-29 10:30:00" },
+    { name: "Geoffrey Bachelet", talk: "Docker", searchable: "geoffrey bachelet ubermuda docker", date: "2014-10-29 10:30:00" },
+    { name: "William Durand", talk: "REST", searchable: "william durand symfony2 rest couac", date: "2014-10-29 10:30:00" }
+];
+
+var items = [];
+for (var i in data) {
+    items.push({
+        properties: {
+            itemId: i,
+            searchableText: data[i].searchable
+        },
+        name: {
+            text: data[i].name
+        },
+        talk: {
+            text: data[i].talk
+        }
+    });
+
+    $.section.setItems(items);
+}
